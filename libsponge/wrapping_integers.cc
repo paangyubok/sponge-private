a1f98b8f88733e47c3813b5c40d21753a7f8a8ac
paangyubok <2837839125@qq.com>
2022-08-16 21:33:43 +0800

    finish `wrap` in lab2

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,4 +1,7 @@
 #include "wrapping_integers.hh"
+#include <algorithm>
+#include <cstdint>
+#include <cstdlib>
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -14,8 +17,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t ret = (n + isn.raw_value()) & 0xffffffff;
+    return WrappingInt32{static_cast<uint32_t>(ret)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +32,13 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    const auto crsn = wrap(checkpoint, isn);
+    const auto diff = n - crsn;
+    const uint32_t floor = isn.raw_value();
+    uint64_t ret{checkpoint};
+    ret += diff;
+    if (ret < floor || (diff < 0 && ret > checkpoint)) {
+        ret = checkpoint + static_cast<uint32_t>(diff);
+    }
+    return ret;
 }
