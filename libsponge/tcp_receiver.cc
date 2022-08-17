b106e150d5b78e9f26b901a762851c46f3b1f8ef
paangyubok <2837839125@qq.com>
2022-08-17 11:31:16 +0800

    ignore the segment from the outside of window and add some comments.

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -18,18 +18,30 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     const bool syn = header.syn;
     const bool fin = header.fin;
     
+    // initialize or exit if tcp is uninitialized
     if (syn) {
         _isn.emplace(header.seqno);
     } else if (!_isn.has_value()) {
         return;
     }
     
+    // get absolute sequence number
     auto abs_sn = unwrap(header.seqno, _isn.value(), _expect);
-    if (_expect >= abs_sn + seg.length_in_sequence_space()) return;
+    
+    // ignore the segment from the outside of window
+    if (_expect >= abs_sn + seg.length_in_sequence_space() ||
+        abs_sn >= _expect + window_size()) {
+        return;
+    }
+    
+    // translate absolute sequence number to stream index
     size_t index = abs_sn > 0 ? abs_sn - 1 : 0;
+    
+    // push payload to reassembler
     _reassembler.push_substring(payload.copy(), 
         index, fin);
         
+    // update _expect
     _expect = _reassembler.stream_out().bytes_written() 
         + 1 + (_reassembler.stream_out().input_ended()? 1:0);
     _ackno.emplace(wrap(_expect, _isn.value()));
@@ -37,4 +49,4 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
 
 optional<WrappingInt32> TCPReceiver::ackno() const { return _ackno; }
 
-size_t TCPReceiver::window_size() const { return _capacity - _reassembler.stream_out().buffer_size(); }
+size_t TCPReceiver::window_size() const { return _reassembler.stream_out().remaining_capacity(); }
